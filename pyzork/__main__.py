a9899b635dc7989f392f82b37291e1a009870eb1
Kendandra <NathanMRojas@gmail.com>
2022-03-28 23:16:21 -0400

    Basic display working.

--- pyzork/__main__.py
+++ pyzork/__main__.py
@@ -4,10 +4,6 @@ from .helpers import clear_screen;
 import importlib.resources as resources
 import json
 
-from .deps.climage import convert
-
-
-
 def main():
 
     clear_screen()
@@ -15,20 +11,25 @@ def main():
     config_json = resources.read_text(data, 'data.json')
 
     room_data = load_room_data(config_json)
+    command_data = load_command_data(config_json)
 
     #current_scene = swap_scene("menu", "title")
     #current_scene.set_scene()
     #result = current_scene.run_scene()
 
-    current_scene = swap_scene("game", room_data[0])
+    command_data = load_command_data(config_json)
+    current_scene = swap_scene("game", room_data[0], command_data)
     current_scene.set_scene()
     result = current_scene.run_scene()
 
 
-def swap_scene(scene_type, scene_data):
+def swap_scene(scene_type, scene_data, command_data):
     clear_screen()
 
-    return Scene(scene_data)
+    return Scene(scene_data, command_data)
 
 def load_room_data(json_data):
     return json.loads(json_data)["rooms"]
+
+def load_command_data(json_data):
+    return json.loads(json_data)["commands"]
--- pyzork/scene.py
+++ pyzork/scene.py
@@ -5,15 +5,20 @@ from .deps.climage import convert
 
 
 class Scene:
-    def __init__(self, data):
+    def __init__(self, data, command_list_data):
         self.name = data["name"]
         self.id = data["id"]
         self.description = data["description"]
         self.image = data["image"]
+        self.commands = data["commands"]
+        self.command_list_data = command_list_data
 
     def set_scene(self):
-        screne_text = self.get_screen_display()
-        print(screne_text)
+        screen_description = self.description
+        screen_visual = self.get_screen_display()
+        command_list = [self.get_command_description(command["type"]) for command in self.commands]
+
+        print(f"{screen_description}\n\n{screen_visual}\n{command_list}")
 
     def run_scene(self):
         self.check_std_opts(input())
@@ -27,5 +32,9 @@ class Scene:
         # return resources.read_text(screen_data, f'{self.name}.pzk')
         # if display type == img
         image = self.image
+        return convert(f'pyzork/data/maps/{image}.png', is_unicode=True, palette="default", width=50)
 
-        return convert(f'pyzork/data/maps/{image}.png', is_unicode=True, palette="default")
+    def get_command_description(self, command_type):
+        command = [command for command in self.command_list_data if command["type"] == command_type]
+        return command[0]["description"]
+
